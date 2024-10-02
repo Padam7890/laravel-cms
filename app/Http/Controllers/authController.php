956c8a7573bbbfe5ead731d90fe67d058e188812
Padam7890 <padamthapamoho@gmail.com>
2024-10-02 14:05:14 +0545

    auth added

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -2,12 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class authController extends Controller
 {
-    
+    public function signup(Request $request){
+        $validate = Validator::make($request->all(), [
+            "name"=> "string|required",
+            "email"=> "email|required|unique:users",
+            "password"=> "string|required|min:8",
+            ]);
+        if($validate->fails()){
+            return response()->json($validate->errors(), 400);
+        }
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        return response()->json([
+            "message" => "User created successfully",
+            "data" => $user
+        ], 201);
+
+    }
     public function login(Request $request) {
-         return "Welcome to the LoginController";
+        $validate = Validator::make($request->all(), [
+            "email"=> "email|required",
+            "password"=> "string|required",
+            ]);
+
+        if($validate->fails()){
+            return response()->json($validate->errors(), 400);
+        }
+
+        if(Auth::attempt(["email"=>$request->email, "password"=>$request->password]))
+        {
+         /** @var \App\Models\MyUserModel $user **/
+            $user = Auth::user();
+            $token = $user->createToken("token")->plainTextToken;
+            return response()->json([
+                "message" => "User authenticated successfully",
+                "access_token" => $token,
+                "token_type" => "Bearer"
+            ], 200);
+        }
+        else {
+            return response()->json([
+                "message" => "Invalid email or password"
+            ], 401);
+        }
+
+    }
+
+    public function logout(Request $request) {
+        $user = $request->user();
+        $user->tokens()->delete();
+        return response()->json([
+            "message" => "User logged out successfully"
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\authController;
 use App\Http\Controllers\EmployeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,10 +19,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get("/employee", [EmployeController::class, "index"]);
+Route::post("/auth/login", [authController::class, "login"]);
+Route::post("/auth/signup", [authController::class, "signup"]);
+
+
+Route::get("/employee", [EmployeController::class, "index"])->name('employee');
 Route::get("/employee/{id}", [EmployeController::class, "show"]);
 
 Route::post('/employees', [EmployeController::class, 'store']);
 
 Route::delete('/employees/{id}', [EmployeController::class,"destroy"]);
+
+
+//Route::apiResource('post', EmployeController::class);
 
